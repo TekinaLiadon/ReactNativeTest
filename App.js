4fa4b8c3265c3da8a3d6ff9706a2c78810def783
TekinaLiadon <64956660+TekinaLiadon@users.noreply.github.com>
2021-12-24 10:20:40 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View,} from 'react-native';
+import CardsDeck from './component/deck/cardsDeck'
+import {NativeBaseProvider, Box} from 'native-base';
+import Navbar from './component/navbar/navbar'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './component/pages/home'
+import TaroDeck from './component/pages/taroDeck'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+
+const App = () => {
+    return (
+        <NativeBaseProvider>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                        <Stack.Screen name="Home" component={Home}
+                                      options={{ title: 'Колода' }}/>
+                        <Stack.Screen name="TaroDeck" component={TaroDeck}
+                                      options={{ title: 'Карты' }}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+            </NativeBaseProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            justifyContent: 'center',
+        },
+    })
+;
+
+export default App
